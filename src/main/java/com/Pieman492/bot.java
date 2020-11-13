0d9d0e10fefc9d004cd863b6dfc28091ff9d2ac0
BuildTools <unconfigured@null.spigotmc.org>
2020-11-13 14:47:40 -0600

    Code establishes connection and brings the bot online

--- src/main/java/com/Pieman492/bot.java
+++ src/main/java/com/Pieman492/bot.java
@@ -1,7 +1,14 @@
 package com.Pieman492;
 
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+
 public class bot {
     public static void main (String[] args) {
-
+        GatewayDiscordClient client = DiscordClientBuilder.create("Nzc2NjAyMTg2MjAwNzc2NzA1.X63RJQ.BMiyD_YQWp6cDd3Ii_UoJYPmvHM")
+                .build()
+                .login()
+                .block();
+        client.onDisconnect().block();
     }
 }
