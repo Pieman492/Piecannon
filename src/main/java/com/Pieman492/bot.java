7a4cd0ec90ae607579b9401ade05511bf6c0fedd
BuildTools <unconfigured@null.spigotmc.org>
2020-11-13 14:57:45 -0600

    Token now not included within published code >.<

--- src/main/java/com/Pieman492/bot.java
+++ src/main/java/com/Pieman492/bot.java
@@ -5,7 +5,7 @@ import discord4j.core.GatewayDiscordClient;
 
 public class bot {
     public static void main (String[] args) {
-        GatewayDiscordClient client = DiscordClientBuilder.create("Nzc2NjAyMTg2MjAwNzc2NzA1.X63RJQ.BMiyD_YQWp6cDd3Ii_UoJYPmvHM")
+        GatewayDiscordClient client = DiscordClientBuilder.create(APIToken.token)
                 .build()
                 .login()
                 .block();
