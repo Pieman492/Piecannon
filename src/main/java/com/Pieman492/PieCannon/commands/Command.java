3de84c7742098021bd50772179f88da44480c655
pieman492 <rauljrrauljr@gmail.com>
2021-02-13 09:55:13 -0600

    - Changed access of members to protected from public. - Overrides toString and returns command prefix

--- src/main/java/com/Pieman492/PieCannon/commands/Command.java
+++ src/main/java/com/Pieman492/PieCannon/commands/Command.java
@@ -4,12 +4,17 @@ import discord4j.core.GatewayDiscordClient;
 
 public abstract class Command {
 
-    protected String commandPrefix;
-    public final char COMMAND_SYMBOL = '!'; // Hardcoded, fix this later;
-    public char getCOMMAND_SYMBOL() {
+    protected String commandPrefix = "";
+    protected final char COMMAND_SYMBOL = '!'; // Hardcoded, fix this later;
+    protected char getCOMMAND_SYMBOL() {
         return COMMAND_SYMBOL;
     }
 
     protected abstract void setCommandPrefix();
     protected abstract void establishCommandAgent(GatewayDiscordClient client);
+
+    @Override
+    public String toString() {
+        return this.commandPrefix;
+    }
 }
