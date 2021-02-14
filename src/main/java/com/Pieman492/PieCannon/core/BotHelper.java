58d8b5c36b962ff3629b76421c63f263abd02e71
pieman492 <rauljrrauljr@gmail.com>
2021-02-14 08:54:00 -0600

    - Converted comment on dumbness to Javadoc comment

--- src/main/java/com/Pieman492/PieCannon/core/BotHelper.java
+++ src/main/java/com/Pieman492/PieCannon/core/BotHelper.java
@@ -1,12 +1,3 @@
-/*
- *  This is super not good practice
- *  but it's my personal project so I'm doing it anyway.
- *  This is a helper class of static methods that do things like
- *  get the API token and generate a random delay.
- *  I might move these into more specialized classes later
- *  but for now they're here to clean up main()
- */
-
 package com.Pieman492.PieCannon.core;
 
 import java.io.FileInputStream;
@@ -15,6 +6,15 @@ import java.time.Duration;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Pattern;
+
+/**
+ *  This is super not good practice
+ *  but it's my personal project so I'm doing it anyway.
+ *  This is a helper class of static methods that do things like
+ *  get the API token and generate a random delay.
+ *  I might move these into more specialized classes later
+ *  but for now they're here to clean up main()
+ */
 
 public final class BotHelper {
 
